e3eb742e68c8b1ea81a07c3cd5f1885d543c99a9
Parvathi <pava@uw.edu>
2021-02-18 13:56:34 -0800

    template

---
+++ app/src/main/java/com/codepath/journal/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.codepath.journal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button createAccountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton=findViewById(R.id.email_sign_in_button);
+        createAccountButton=findViewById(R.id.create_account_button);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, CreateAccount.class));
+            }
+        });
+
+
+    }
+}
