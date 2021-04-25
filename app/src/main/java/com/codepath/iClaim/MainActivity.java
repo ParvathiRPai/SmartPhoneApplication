2ffab82c4c9a294496c1c135c63ed246c5139999
krishnajha99 <krjha@adobe.com>
2021-04-25 13:26:40 -0700

    check-int bot lib for iClaim-3

--- app/src/main/java/com/codepath/iClaim/MainActivity.java
+++ app/src/main/java/com/codepath/iClaim/MainActivity.java
@@ -2,25 +2,53 @@ package com.codepath.iClaim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
+import com.bot.codepath.Chatbot;
+import com.bot.codepath.ChatbotActivity;
+import com.bot.codepath.ChatbotSettings;
+import com.bot.codepath.DialogflowCredentials;
+
+import java.util.UUID;
+
+
 public class MainActivity extends AppCompatActivity {
-    private Button getStartedButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getStartedButton=findViewById(R.id.startButton);
-        getStartedButton.setOnClickListener(new View.OnClickListener() {
+
+        Button chatFab = findViewById(R.id.chatFab);
+        chatFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-
+                openChatbot(view);
             }
         });
+    }
+
+    public void openChatbot(View view) {
+        // provide your Dialogflow's Google Credential JSON saved under RAW folder in resources
+        DialogflowCredentials.getInstance().setInputStream(getResources().openRawResource(R.raw.auth));
+
+        ChatbotSettings.getInstance().setChatbot( new Chatbot.ChatbotBuilder()
+//                .setDoAutoWelcome(false) // True by Default, False if you do not want the Bot to greet the user Automatically. Dialogflow agent must have a welcome intent to handle this
+//                .setChatBotAvatar(getDrawable(R.drawable.avatarBot)) // provide avatar for your bot if default is not required
+//                .setChatUserAvatar(getDrawable(R.drawable.avatarUser)) // provide avatar for your the user if default is not required
+                .setShowMic(true) // False by Default, True if you want to use Voice input from the user to chat
+                .build());
+        Intent intent = new Intent(MainActivity.this, ChatbotActivity.class);
+        Bundle bundle = new Bundle();
+
+        // provide a UUID for your session with the Dialogflow agent
+        bundle.putString(ChatbotActivity.SESSION_ID, UUID.randomUUID().toString());
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
